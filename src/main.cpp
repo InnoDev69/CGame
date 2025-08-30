a121d08e95b62e3ee08ac31cf9868539ca229329
InnoDev69 <yamirnu14@gmail.com>
2025-08-29 23:54:57 -0300

    Changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,10 @@
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-#include "render.h"
+#include "game.h"
 
 int main()
 {
-    sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
-
-    sf::RenderWindow window(desktopMode, "SFML works!", sf::Style::Titlebar);
-    sf::CircleShape shape(100.f);
-    Render render(2,2);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    Game game(800, 600, "CGame - SFML");
+    game.run();
+    return 0;
 }
